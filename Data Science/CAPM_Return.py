c4ef322570ef257c4bd3c7ea4f3bc0dadbd9fceb
Ganeshasoham001 <112200428+Ganeshasoham001@users.noreply.github.com>
2023-09-05 21:11:05 +0530

    CAPM_Return.py

--- Data Science/CAPM_Return.py
+++ Data Science/CAPM_Return.py
@@ -4,7 +4,8 @@ import numpy as np
 import yfinance as yf
 import datetime as dt
 import pandas_datareader.data as pdr
-import capm_functions
+import plotly.express as px
+import numpy as np
 st.set_page_config(page_title= "CAPM",
                    page_icon= "chart_with_upward_trends",
                    layout= 'wide')
@@ -85,7 +86,33 @@ try:
         st.dataframe(return_df,use_container_width=True)
 except:
     st.write('Please select Valid stocks')
+def interactive_plot(df):
+    fig= px.line()
+    for i in df.columns[1:]:
+        fig.add_scatter(x=df['Date'], y= df[i],name= i)
+    fig.update_layout(width=450,margin=dict(l=20,r=20,t=60,b=20),legend= dict(orientation='h',yanchor='bottom',y=1.02,xanchor='right',x =1,))
+    return fig
+# function for normalize of prices
+def normalized(df_2):
+    df=df_2.copy()
+    for i in df.columns[1:]:
+        df[i]=df[i]/df[i][0]
+    return df
+#functions to calculate daily returns
+def daily_returns(df):
+    df_daily_rtr=df.copy()
+    for i in df.columns[1:]:
+        for j in range(1,len(df)):
+            df_daily_rtr[i][j]= ((df[i][j]-df[i][j-1])/df[i][j-1])*100
+        df_daily_rtr[i][0]=0
+    return df_daily_rtr
+ 
 
+
+def calculate_beta(stocks_daily_rtr,stock):
+    rm= stocks_daily_rtr['sp500'].mean()*252
+    b,a=np.polyfit(stocks_daily_rtr['sp500'],stocks_daily_rtr[stock],1)
+    return b,a
                           
                           
 
